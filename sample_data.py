b336758df021522bf332b26caa6d7db0fe8270a9
valnam4bite <valnam@ukr.net>
2026-07-11 18:08:04 +0300

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,47 @@
+from album import Album
+
+albums = [
+
+    Album(
+        "Kind of Blue",
+        "Miles Davis",
+        "Vinyl",
+        5
+    ),
+
+    Album(
+        "Abbey Road",
+        "The Beatles",
+        "Smartphone",
+        5
+    ),
+
+    Album(
+        "Discovery",
+        "Daft Punk",
+        "Desktop",
+        5
+    ),
+
+    Album(
+        "Dummy",
+        "Portishead",
+        "Vinyl",
+        5
+    ),
+
+    Album(
+        "Kid A",
+        "Radiohead",
+        "Desktop",
+        4
+    ),
+
+    Album(
+        "Rumours",
+        "Fleetwood Mac",
+        "Smartphone",
+        5
+    )
+
+]
