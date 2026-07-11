535a20cd5387dbe31ef5f8e62a6260eefb999ca6
valnam4bite <valnam@ukr.net>
2026-07-11 18:10:48 +0300

    Refactor main.py to use Catalog and show_statistics

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-""" Music collection catalog """
+from catalog import Catalog
+from sample_data import albums
+from statistics import show_statistics
 
-print("Project: music-catalog")
+catalog = Catalog()
+
+for album in albums:
+    catalog.add(album)
+
+print("=" * 40)
+print("MUSIC COLLECTION")
+print("=" * 40)
+
+for album in catalog.all():
+    print(album)
+    print()
+
+show_statistics(catalog.all())
